434d4925afbdbf164a5859bd0a50d958c2cb6560
Galib Mammadli <qalib.mmmdli@gmail.com>
2025-11-19 01:22:42 +0400

    refactor: project structure refactored, added new folders and files for data and types

---
+++ src/types/data.type.ts
@@ -0,0 +1,26 @@
+export interface Country {
+    id: number;
+    name: string;
+};
+
+export interface Hotel {
+    id: number;
+    name: string;
+    price: number;
+};
+
+export interface BoardType {
+    code: string;
+    name: string;
+};
+
+export interface Meal {
+    id: number;
+    name: string;
+    price: number;
+}
+
+export interface MealType {
+    dinner: Meal[];
+    lunch: Meal[];
+}
